798088a9131c83c5b276a3566ac73ca62739e745
Davi <sbernardo.davi@outlook.com>
2021-12-03 13:34:34 -0300

    Feat: Criação dos cards

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,16 @@
+import styles from './Cards.module.css'
+
+function Cards({Produto}){
+    return(
+        <div>
+
+            <div class={styles.aindamais}>
+                    <button> Ainda mais produtos aqui!</button>
+                </div>
+      
+        </div>
+        
+    )
+}
+
+export default Cards
